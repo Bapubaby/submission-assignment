0aa471e630bd980c81be6438d521e53e45c200a8
Bapubaby <nimumehta83@gmail.com>
2020-10-15 13:03:33 +0530

    Assignment Completed

--- app/navigators/AppNavigator.js
+++ app/navigators/AppNavigator.js
@@ -8,48 +8,16 @@ import RootScreen from '../scenes/RootScreen';
 import FavouritesScreen from '../scenes/FavouritesScreen';
 import DetailsScreen from '../scenes/DetailsScreen';
 
-// const DetailsStack = createStackNavigator({
-//   Favourites: {
-//     screen: ExampleScreen
-//   },
-//   {
-
-//   }
-// });
 const StackNavigator = createStackNavigator(
   {
-    // Create the application routes here (the key is the route name, the value is the target screen)
-    // See https://reactnavigation.org/docs/en/stack-navigator.html#routeconfigs
     FavouritesScreen,
-    // The main application screen is our "ExampleScreen". Feel free to replace it with your
-    // own screen and remove the example.
     MainScreen: DetailsScreen
   },
   {
-    // By default the application will show the splash screen
     initialRouteName: 'FavouritesScreen',
-    // See https://reactnavigation.org/docs/en/stack-navigator.html#stacknavigatorconfig
     headerMode: 'none'
   }
 );
-// const DetailsStackScreen = () => (
-//   <DetailsStack.Navigator>
-//     <DetailsStack.Screen
-//       name="Favourites"
-//       component={ExampleScreen}
-//       options={{
-//         headerTitle: 'Favourites'
-//       }}
-//     />
-//     <DetailsStack.Screen
-//       name="BankDetails"
-//       component={DetailsScreen}
-//       options={({ route }) => ({
-//         title: `${route.params.data.ifsc}`
-//       })}
-//     />
-//   </DetailsStack.Navigator>
-// );
 
 const AppTabs = createBottomTabNavigator(
   {
